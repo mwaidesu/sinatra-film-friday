805b62b2c401b9f06eca471d48c5354584ae2800
Mwai <machariamwai@gmail.com>
2022-06-30 17:02:49 +0300

    Change key: Daily limit reached

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -1,31 +1,31 @@
 class MovieController < ApplicationController
-  get "/search" do
-    redirect "movies/search"
+  get '/search' do
+    redirect 'movies/search'
   end
 
-  get "/movies/search" do
+  get '/movies/search' do
     erb :'movies/search'
   end
 
-  post "/movies" do
+  post '/movies' do
     # params cannot be empty
-    if params["title"].empty?
-      flash[:message] = "Error: Search field must be filled."
-      redirect to "/movies/search"
+    if params['title'].empty?
+      flash[:message] = 'Error: Search field must be filled.'
+      redirect to '/movies/search'
     end
 
     title = params[:title]
-    url = "http://www.omdbapi.com/?s=#{title}&plot=full&apikey=cc4fb477"
+    url = "http://www.omdbapi.com/?s=#{title}&plot=full&apikey=9df5717"
 
     results = JSON.load(open(url).read)
-    @movies = results["Search"]
+    @movies = results['Search']
     erb :'movies/results'
-  end 
+  end
 
-  post "/movies/results" do
-    if !logged_in?(session)
-      flash[:message] = "You must be logged in to review a movie."
-      redirect "/"
+  post '/movies/results' do
+    unless logged_in?(session)
+      flash[:message] = 'You must be logged in to review a movie.'
+      redirect '/'
     end
 
     @movie = get_movie_detail(params[:imdbID])
@@ -35,41 +35,40 @@ class MovieController < ApplicationController
 
     if !found_movie || !current_user.movies.include?(found_movie)
 
-    #  @new_movie = Movie.find_or_create_by(imdbID: params[:imdbID])
+      #  @new_movie = Movie.find_or_create_by(imdbID: params[:imdbID])
       @new_movie = Movie.create(imdbID: params[:imdbID])
 
-      @new_movie.title = @movie["Title"]
-      @new_movie.plot = @movie["Plot"]
-      @new_movie.actors = @movie["Actors"]
-      @new_movie.released = @movie["Released"]
-      @new_movie.runtime = @movie["Runtime"]
-      @new_movie.genre = @movie["Genre"]
-      @new_movie.rated = @movie["Rated"]
-      @new_movie.poster_url = @movie["Poster"]
+      @new_movie.title = @movie['Title']
+      @new_movie.plot = @movie['Plot']
+      @new_movie.actors = @movie['Actors']
+      @new_movie.released = @movie['Released']
+      @new_movie.runtime = @movie['Runtime']
+      @new_movie.genre = @movie['Genre']
+      @new_movie.rated = @movie['Rated']
+      @new_movie.poster_url = @movie['Poster']
       @new_movie.save
       current_user.movies << @new_movie
     else
-      flash[:message] = "You have already reviewed this movie."
-      redirect "/movies/search"
+      flash[:message] = 'You have already reviewed this movie.'
+      redirect '/movies/search'
     end
     erb :'reviews/new'
   end
 
-  #read movie details via OMDB
-  get "/movies/imdb/:movieID" do
+  # read movie details via OMDB
+  get '/movies/imdb/:movieID' do
     imdbID = params[:movieID]
     @details = get_movie_detail(imdbID)
     erb :'movies/details'
   end
 
-  get "/movies/:slug" do
+  get '/movies/:slug' do
     @movie = Movie.find_by_slug(params[:slug])
     erb :'movies/show'
   end
 
   def get_movie_detail(id)
-    url = "http://www.omdbapi.com/?i=#{id}&plot=full&apikey=cc4fb477"
+    url = "http://www.omdbapi.com/?i=#{id}&plot=full&apikey=9df5717"
     results = JSON.load(open(url).read)
   end
-  
 end
